8d6e848c7faa59e78447401879ee1f16249667ff
Lawrence Shi <lishi25@berkeley.edu>
2026-01-13 16:07:54 -0600

    Rolling volatility

--- src/data_eda.py
+++ src/data_eda.py
@@ -1,6 +1,6 @@
 import sqlite3
 import pandas as pd
-
+import numpy as np
 
 def get_product_data(product_id):
     con = sqlite3.connect('../data/bazaar.db')
@@ -25,10 +25,36 @@ def get_all_data():
 
 
 
-def get_info(item):
+def get_info(item, window=0):
     df = get_product_data(item)
+
+    assert(window >= 0, "bruh")
+    if window:
+        df = df.rolling(window)
+
     summary = df[['buy_price', 'buy_volume', 'sell_price', 'buy_volume']].describe()
+    print(type(summary))
     print(summary)
     return summary
 
 
+
+get_info('ENCHANTED_GOLD')
+
+def returns(df):
+    df['log_returns'] = np.log(df['buy_price'] / df['buy_price'].shift(1))
+
+
+#check for
+    results = df['timestamp'].apply(valid_entry, args=(df['timestamp'] .shift(1),)).dropna()
+    valid = results.all()
+
+
+
+def valid_entry(curr, prev):
+    return curr < prev + 90000
+
+sum = get_info('ENCHANTED_GOLD')
+mean_b_iloc = sum.iloc[0, 0]
+
+print(mean_b_iloc)
--- src/visialization.py
+++ src/visialization.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
-from data_eda import get_product_data, get_all_data
+from data_eda import get_product_data, get_all_data, get_info
 
 
 def price_trend(product_id):
@@ -36,10 +36,10 @@ def spread(product_id):
 
     plt.figure(figsize=(10, 5))
 
-    df = df[(df['net_spread'] > -10000) & (df['net_spread'] < 100000)]
+    #df = df[(df['net_spread'] > -10000) & (df['net_spread'] < 100000)]
 
-    plt.ticklabel_format(style='plain', axis='x') #NOTE
-    plt.hist(df['net_spread'].dropna(), bins=300, color='skyblue', edgecolor='black')
+    #plt.ticklabel_format(style='plain', axis='x') #NOTE
+    plt.hist(df['net_spread'].dropna(), bins=30, color='skyblue', edgecolor='black')
 
     # Add a vertical line for the mean
     plt.axvline(df['net_spread'].mean(), color='red', linestyle='dashed', linewidth=1, label='Mean Spread')
@@ -50,6 +50,25 @@ def spread(product_id):
     plt.legend()
     plt.show()
 
+def scatter(product_id):
+    df = get_product_data(product_id)
 
-spread("ENCHANTED_GOLD")
+    df.plot.scatter(x='buy_volume', y='buy_price')  # Use c for color mapping
+    plt.show()
 
+def rolling_volatility(product_id):
+    df = get_product_data(product_id)
+
+    df['moving_std_hour'] = df['buy_price'].rolling(window=60).std()
+    if get_info(product_id).iloc[0, 0] < 1440:
+        mu = df['buy_price'].mean()
+    else:
+        mu = df['buy_price'].rolling(window=1440).mean()
+    df['z_score'] = (df['buy_price'] - mu) / df['moving_std_hour']
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(df.index, df['z_score'], label='Buy Price z score')
+    plt.show()
+
+# scatter("ENCHANTED_GOLD")
+rolling_volatility("ENCHANTED_GOLD")
